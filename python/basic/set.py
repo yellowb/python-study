ee1fd0fde9ce4b04b70757cffb3502de090066e6
yellowb <122517167@qq.com>
2017-06-10 20:43:11 +0800

    update samples

--- python/basic/set.py
+++ python/basic/set.py
@@ -15,3 +15,11 @@ set1.remove(1)
 print(set1)
 set1.remove((1, 2))
 print(set1)
+
+# 交集并集
+set2 = {1, 2, 3}
+set3 = {1, 2, 4}
+print(set2 & set3)
+print(set2 | set3)
+print(set2 - set3)
+print(set2 ^ set3)
