df58a8b3088c2e2ab5048ce75c4abb171baf9172
yellowb <122517167@qq.com>
2017-06-15 18:55:42 +0800

    update

--- pandas_/series.py
+++ pandas_/series.py
@@ -3,7 +3,9 @@ import numpy as np
 
 data = np.arange(0, 1000)
 _1_d_array = Series(data)
-print(_1_d_array.describe())
+print(_1_d_array.index)
 
-map1 = Series([1, 2, 3], index=['a', 'b', 'c'])
-print(map1['a'])
+map1 = Series([1, 2, 3])
+print(map1.index)
+
+
