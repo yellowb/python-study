b93d31dd48baa3a072dc730cefaa9845cf600ee5
yellowb <122517167@qq.com>
2017-06-18 23:33:12 +0800

    update

--- dsh/bl_freight_regression_model_GridSearch.py
+++ dsh/bl_freight_regression_model_GridSearch.py
@@ -3,6 +3,9 @@ import numpy as np
 # 交叉验证器
 from sklearn.model_selection import cross_val_score
 
+# 超参数搜索
+from sklearn.model_selection import GridSearchCV
+
 # 模型
 from sklearn.linear_model import SGDRegressor
 from sklearn.linear_model import Ridge
@@ -19,6 +22,7 @@ from sklearn.metrics import r2_score
 
 from pandas import DataFrame
 import pandas as pd
+import time
 
 
 def do():
@@ -30,21 +34,17 @@ def do():
     train_data = train_data.drop(drop_col_names, axis=1)
     test_data = test_data.drop(drop_col_names, axis=1)
 
+    # Drop the 1st no-meaning column
     train_data = train_data.drop(train_data.columns[0], axis=1)
     test_data = test_data.drop(test_data.columns[0], axis=1)
 
-
-
-
+    # limit the TIME_USED of data
     train_data = train_data[train_data["TIME_USED"] <= 1000]
     test_data = test_data[test_data["TIME_USED"] <= 1000]
 
-
+    # change second -> minute
     train_data['TIME_USED'] = train_data['TIME_USED'] / 60
     test_data['TIME_USED'] = test_data['TIME_USED'] / 60
-
-
-
 
     print(train_data.head())
 
@@ -59,34 +59,40 @@ def do():
     # regressor = SGDRegressor(l1_ratio=0.1)
     # regressor = Ridge()
     # regressor = SVR()
-    regressor = RandomForestRegressor()
+    regressor = RandomForestRegressor(n_jobs=-1)
     # regressor = AdaBoostRegressor()
     # regressor = GradientBoostingRegressor(n_estimators=400, max_depth=4, loss='huber')
     # regressor = BaggingRegressor()
 
-    # 用训练集做交叉验证
-    # scores = cross_val_score(regressor, X_train, y_train, cv=5, scoring='r2', n_jobs=-1)
+    # 超参数搜索空间
+    param_grid = {
+        'criterion': ['mse', 'mae'],
+        'n_estimators': [10, 20, 50, 100, 200, 300, 400, 500, 750, 1000],
+        'max_features': ['auto', 'sqrt', 'log2']
+    }
 
-    # print('交叉验证R方值:', scores)
-    # print('交叉验证R方均值:', np.mean([scores]))
+    grid_search = GridSearchCV(estimator=regressor, param_grid=param_grid, n_jobs=-1, verbose=1,
+                               scoring='neg_mean_absolute_error', cv=4)
 
-    # 用训练集训练模型
-    regressor.fit(X_train, y_train)
-    # 用模型预测测试集, 打分方法也是r2
-    print('测试集R方值:', regressor.score(X_test, y_test))
+    # 搜索超参数组合
+    grid_search.fit(X_train, y_train)
+    print('最佳效果：%0.3f' % grid_search.best_score_)
+    print('最优参数组合：')
+    best_parameters = grid_search.best_estimator_.get_params()
 
-    # 对比预测数据与真实数据
-    y_predict = regressor.predict(X_test);
-    df = DataFrame()
-    df['predict'] = y_predict
-    df['real'] = y_test
-    df['diff'] = y_predict - y_test
-    print(df.head(20))
+    # 输出最佳超参数
+    for param_name in sorted(param_grid.keys()):
+        print('\t%s: %r' % (param_name, best_parameters[param_name]))
 
+    # 用最佳超参数预测测试集
+    y_predict = grid_search.predict(X_test)
     print('MAE =  ', mean_absolute_error(y_test, y_predict))
     print('MSE =  ', mean_squared_error(y_test, y_predict))
     print('R2 =  ', r2_score(y_test, y_predict))
 
 
 if __name__ == '__main__':
+    start_time = time.time()
     do()
+    stop_time = time.time()
+    print('用时: ', (stop_time - start_time), ' (s)')
