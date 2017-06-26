2a6626306a8464421972688e02daf5f5ae21e019
yellowb <122517167@qq.com>
2017-06-26 23:15:04 +0800

    update

--- dsh/gen_permutations.py
+++ dsh/gen_permutations.py
@@ -1,7 +1,32 @@
 from itertools import permutations
+from pandas import DataFrame
+import pandas as pd
+import numpy as np
 
 list = ['P1', 'P2', 'P3']
 gen = permutations(list, len(list))
 
 for line in gen:
     print(line)
+
+performance_matrix = np.array([
+    [2, 5, 3],
+    [2, 1, 4],
+    [4, 3, 1],
+    [3, 2, 1],
+    [4, 5, 8],
+    [7, 4, 4]
+])
+
+performance_matrix_df = DataFrame(performance_matrix,
+                                  columns=list,
+                                  index=['T1', 'T2', 'T3', 'T4', 'T5', 'T6'])
+
+sub_df = performance_matrix_df.iloc[3:6]
+
+# print(performance_matrix_df.iloc[0:3])
+
+sub_df['P1'] += 2
+
+
+print(performance_matrix_df[:6])
